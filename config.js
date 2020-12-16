9f93a9a7e907269d73cce6e0935e5b11a563f3e0
Jacob W Anderson <37127146+jake22anderson@users.noreply.github.com>
2020-12-15 19:04:10 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Open your spotify dashboard
+ * https://beta.developer.spotify.com/dashboard
+ */
+
+const client_id = '551ed56d58cb40a6ba0b5d8e9522de2f';
+const redirect_uri = 'http://localhost:1410/';
+const client_secret = 'edbc95099b8b464ea8d9880a86ca84b5';
